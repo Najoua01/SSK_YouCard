db500e2569020aa4ec65382d4ae3121764a1ec6d
Najoua01 <najoua@tutamail.com>
2024-06-05 12:06:52 +0200

    corrections signup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,15 +7,29 @@ const saltrounds = 10;
 exports.signupMembre = async (req, res) => {
     const { nom, prenom, email, pays, ville, codePostal, password, dateNaiss } = req.body;
     try {
+        const emailCheckQuery = 'SELECT email FROM users WHERE email = ?';
+        const emailResult = await connection.promise().query(emailCheckQuery, [email]);
+        if (emailResult[0].length > 0) {
+            return res.status(409).json({ error: 'Email déjà utilisé' });
+        }
         const hashedPassword = await bcrypt.hash(password, saltrounds);
-        const query = 'INSERT INTO users (nom, prenom, email, pays, ville, codePostal, password, Roles_Id) VALUES (?, ?, ?, ?, ?, ?, ?, (SELECT Id_Roles FROM roles WHERE role_name = ?))';
-        const values = [nom, prenom, email, pays, ville, codePostal, hashedPassword, 'membre'];
-        connection.query(query, values, (err, result) => {
+        const usersInsertQuery = 'INSERT INTO users (nom, prenom, email, pays, ville, codePostal, password, Roles_Id) VALUES (?, ?, ?, ?, ?, ?, ?, (SELECT Id_Roles FROM roles WHERE role_name = ?))';
+        const usersValues = [nom, prenom, email, pays, ville, codePostal, hashedPassword, 'membre'];
+        connection.query(usersInsertQuery, usersValues, async (err, result) => {
             if (err) {
                 console.error('Erreur lors de l\'insertion dans la base de données :', err);
                 return res.status(500).json({ error: 'Erreur interne du serveur' });
             }
-            res.status(200).json({ message: 'Inscription membre réussie!' });
+            const users_Id = result.insertId;
+            const membresInsertQuery = 'INSERT INTO membres (dateNaiss, Users_Id) VALUES (?,?)';
+            const membresValues = [dateNaiss, users_Id];
+            await connection.query(membresInsertQuery, membresValues, (err, membresResult) => {
+                if (err) {
+                    console.log('Erreur lors de l\'insertion dans la table membres :', err);
+                    return res.status(500).json({ error: 'Erreur lors de l\'enregistrement des détails du membre' });
+                }
+                res.status(200).json({message: 'Inscription membre réussie!', users_Id: users_Id});
+            });
         });
     } catch (error) {
         console.error('Erreur lors du hachage du mot de passe :', error);
@@ -26,16 +40,36 @@ exports.signupMembre = async (req, res) => {
 exports.signupPartenaire = async (req, res) => {
     const { nom, prenom, email, pays, ville, codePostal, password, siteUrl, tva, telephone, contactNom, contactPrenom, contactEmail, contactTelephone } = req.body;
     try {
+        const emailCheckQuery = 'SELECT email FROM users WHERE email = ?';
+        const emailResult = await connection.promise().query(emailCheckQuery, [email]);
+        if (emailResult[0].length > 0) {
+            return res.status(409).json({ error: 'Email déjà utilisé' });
+        }
         const hashedPassword = await bcrypt.hash(password, saltrounds);
-        const query = 'INSERT INTO users (nom, prenom, email, pays, ville, codePostal, password, Roles_Id) VALUES (?, ?, ?, ?, ?, ?, ?, (SELECT Id_Roles FROM roles WHERE role_name = ?))';
-        const values = [nom, prenom, email, pays, ville, codePostal, hashedPassword, 'partenaire'];
-        connection.query(query, values, (err, result) => {
+        const usersInsertQuery = 'INSERT INTO users (nom, prenom, email, pays, ville, codePostal, password, Roles_Id) VALUES (?, ?, ?, ?, ?, ?, ?, (SELECT Id_Roles FROM roles WHERE role_name = ?))';
+        const usersValues = [nom, prenom, email, pays, ville, codePostal, hashedPassword, 'partenaire'];
+        connection.query(usersInsertQuery, usersValues, (err, result) => {
             if (err) {
                 console.error('Erreur lors de l\'insertion dans la base de données :', err);
                 return res.status(500).json({ error: 'Erreur interne du serveur' });
             }
-            res.status(200).json({ message: 'Inscription partenaire réussie!' });
+            const users_Id = result.insertId;
+            const partenairesInsertQuery = `
+                INSERT INTO partenaires (siteUrl, tva, telephone, contactNom, contactPrenom, contactEmail, contactTelephone, Users_Id)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            `;
+            const partenairesValues = [siteUrl, tva, telephone, contactNom, contactPrenom, contactEmail, contactTelephone, users_Id];
+            
+            connection.query(partenairesInsertQuery, partenairesValues, (err, partenairesResult) => {
+                if (err) {
+                    console.error('Erreur lors de l\'insertion dans la table partenaires :', err);
+                    return res.status(500).json({ error: 'Erreur lors de l\'enregistrement des détails du partenaire' });
+                }
+                res.status(200).json({message: 'Inscription partenaire réussie!', users_Id: users_Id});
+            });
+            res.status(200).json({ message: 'Inscription partenaire réussie!', users_Id: users_Id });
         });
+
     } catch (error) {
         console.error('Erreur lors du hachage du mot de passe :', error);
         res.status(500).json({ error: 'Erreur interne du serveur' });
